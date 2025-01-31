136006e9fbe289a89ff56bd0221b8f194142a800
Bashar Alwarad <beelwarad52@gmail.com>
2025-01-31 09:03:00 +0100

    users from mongodb

--- db.js
+++ db.js
@@ -21,3 +21,35 @@ connectToDatabase();
 
 const db = client.db(MONGODB_NAME);
 export { db, client };
+
+// import { Sequelize } from 'sequelize';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const NEON = process.env.NEON;
+
+// // Initialize Sequelize
+// export const sequelize = new Sequelize(NEON, {
+//   dialect: 'postgres',
+//   logging: false,
+// });
+
+// // Test the connection
+// (async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log('Database connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// })();
+
+// export const db = async () => {
+//   try {
+//     await sequelize.sync();
+//     console.log('Database synced.');
+//   } catch (error) {
+//     console.error('Error syncing database:', error);
+//   }
+// };
