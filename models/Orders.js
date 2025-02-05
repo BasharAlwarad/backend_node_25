799b6db09025983bbcd6378215918777019b1104
Bashar Alwarad <beelwarad52@gmail.com>
2025-02-05 08:00:46 +0100

    cleaning

--- models/Orders.js
+++ models/Orders.js
@@ -16,36 +16,3 @@ const orderSchema = new mongoose.Schema(
 
 const Order = mongoose.model('Order', orderSchema);
 export default Order;
-
-// import { DataTypes } from 'sequelize';
-// import { sequelize } from '../db.js';
-
-// const Orders = sequelize.define(
-//   'orders',
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     price: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     date: {
-//       type: DataTypes.DATE,
-//       allowNull: false,
-//       defaultValue: DataTypes.NOW,
-//     },
-//     user_id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//   },
-//   {
-//     tableName: 'orders',
-//     timestamps: false,
-//   }
-// );
-
-// export default Orders;
--- models/User.js
+++ models/User.js
@@ -13,37 +13,3 @@ const userSchema = new mongoose.Schema(
 
 const User = mongoose.model('User', userSchema);
 export default User;
-
-// import { DataTypes } from 'sequelize';
-// import { sequelize } from '../db.js';
-
-// const User = sequelize.define(
-//   'users',
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     first_name: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     last_name: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     age: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//   },
-//   {
-//     tableName: 'users',
-//     timestamps: true,
-//     createdAt: 'created_at',
-//     updatedAt: 'updated_at',
-//   }
-// );
-
-// export default User;
